1664247e5391bf41e67afdee7c04788ea773aee8
Demisodaba <demo.nkmt1@gmail.com>
2024-03-25 16:16:03 +0300

    Configure Flask app with email functionality using SMTP port 587

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -22,10 +22,11 @@ login_manager.login_view = 'login'
 UPLOAD_FOLDER = 'books'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Configure email server settings
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')  # Access EMAIL_USER from environment variable
 app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASS')  # Access EMAIL_PASS from environment variable
 mail = Mail(app)
